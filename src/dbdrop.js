833edabd4b6395948678e9569ce30a3755927d09
Jonathan Pool <pool@stulta.com>
2017-07-26 20:07:28 -0700

    Generated comment-sparse files.

--- src/dbdrop.js
+++ src/dbdrop.js
@@ -4,49 +4,33 @@ const {Client} = require('pg');
 
 /// Destruction of tasks database.
 
-fs.readFile('./sql/dbdrop.sql', 'utf8', (err, data) => {
-  if (err) {
-    console.log('Error (./sql/dbdrop.sql): ' + err.message);
+const dropCommands = [
+  'drop database tasks',
+  'drop role taskmaster'
+];
+const client = new Client({database: 'postgres'});
+client.connect().then(
+  () => {
+    console.log('Connected to postgres database');
+    return client.query(dropCommands[0]);
   }
-  else {
-    const client = new Client({database: 'postgres'});
-    client.connect().catch(
-      error => {
-        console.log(
-          'Connection to postgres database failed with message:\n'
-          + error.message
-        );
-      }
-    ).then(
-      () => {
-        console.log('Connected to postgres database');
-        client.query(data);
-      }
-    ).catch(
-      error => {
-        console.log(
-          'Tasks database destruction failed with message:\n' + error.message
-        );
-      }
-    ).then(
-      result => {
-        console.log(
-          'Tasks database and manager role destroyed with messages:\n'
-          + result
-        );
-        client.end();
-      }
-    ).catch(
-      error => {
-        console.log(
-          'Disconnection from postgres database failed with message:\n'
-          + error.message
-        );
-      }
-    ).then(
-      () => {
-        console.log('Disconnected from postgres database');
-      }
-    );
+).then(
+  () => {
+    console.log('Tasks database destroyed');
+    return client.query(dropCommands[1]);
   }
-});
+).then(
+  () => {
+    console.log('Taskmaster role destroyed');
+    return client.end();
+  }
+).then(
+  () => {
+    console.log('Disconnected from postgres database');
+  }
+).catch(
+  error => {
+    console.log('Error (client): ' + error.message);
+    client.end();
+  }
+);
