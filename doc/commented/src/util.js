59fc16b21c6d7f0dad172970f95b36d27bdaffba
Jonathan Pool <pool@stulta.com>
2017-07-27 16:48:11 -0700

    Split task.js to prevent loading for testing from running code.

---
+++ doc/commented/src/util.js
@@ -0,0 +1,163 @@
+/**
+  Management utilities for a list stored in a PostgreSQL database. Performs
+  additions to it, deletions from it, and disclosures and resets of it.
+*/
+
+/// ///// IMPORTS ///// ///
+
+// Import Client from the pg module.
+const {Client} = require('pg');
+
+// Import the “cli-table2” module.
+const Table = require('cli-table2');
+
+/// ///// GENERAL FUNCTIONS ///// ///
+
+/**
+  Define a function that returns a database function invocation.
+  Preconditions:
+    0. args has length 1 or more.
+    1. Each element of args is defined.
+    2. args[0] is a valid PostgreSQL identifier.
+*/
+const mkFnCall = args => {
+  // Identify the function arguments as a string, blank if none.
+  const fnArgs = args.slice(1).map(arg => "'" + arg + "'").join(', ');
+  // Return the query.
+  return 'select * from ' + args[0] + '(' + fnArgs + ')';
+};
+
+/**
+  Define a function that formulates a message.
+  Preconditions:
+    0. messages is an object.
+    1. messageKey is one of the properties of messages.
+    2. symbol is a nonblank string without any RegExp metacharacters.
+    3. replacement is a string.
+*/
+const formulateMessage = (messages, messageKey, symbol, replacement) => {
+  // Initialize the message.
+  let message = messages[messageKey];
+  // If there is a symbol to replace in it:
+  if (symbol) {
+    // Replace all instances of it.
+    message = message.replace(RegExp(symbol, 'g'), replacement);
+  }
+  // Return the message.
+  return message;
+};
+
+/// Define a function that acts on a message.
+const handleMessage = (messages, messageKey, symbol, replacement) => {
+  // Formulate and output the message.
+  console.log(formulateMessage(messages, messageKey, symbol, replacement));
+};
+
+/**
+  Define a function that connects to the tasks database, executes a function
+  in it, handles its result, and disconnects from the database.
+*/
+const callFn = (messages, handler, fnName, ...fnArgs) => {
+  // Create a client to connect to the tasks database.
+  const client = new Client({
+    user: 'taskmaster',
+    database: 'tasks'
+  });
+  // Make the connection. After it is complete:
+  client.connect().then(
+    // If it succeeded:
+    () => {
+      // Execute the specified function and return its result.
+      return client.query(mkFnCall([fnName, ...fnArgs]));
+    }
+  // After the execution is complete:
+  ).then(
+    // If it succeeded:
+    result => {
+      // Handle the result of the function execution.
+      handler(messages, result);
+      // Disconnect from the tasks database.
+      return client.end();
+    }
+  // After the disconnection is complete:
+  ).catch(
+    // If any error was thrown:
+    error => {
+      // Report it.
+      console.log('Error (' + fnName + '): ' + error.message);
+      // Disconnect from the tasks database, if still connected.
+      client.end();
+    }
+  );
+};
+
+/// ///// COMMAND-SPECIFIC HANDLERS ///// ///
+
+/// Define a handler for the result of the help command.
+const helpHandler = messages => {
+  handleMessage(messages, 'helpTip');
+};
+
+/// Define a handler for the result of the add command.
+const addHandler = (messages, result) => {
+  handleMessage(
+    messages, 'addReport', '«addResult»', result.rows[0].identifier
+  );
+};
+
+/// Define a handler for the result of the done command.
+const doneHandler = (messages, result) => {
+  // Identify an array of the row objects in the result.
+  const rows = result.rows;
+  // If any tasks were removed:
+  if (rows.length) {
+    // For each of them:
+    for (const row of rows) {
+      // Handle its values.
+      handleMessage(
+        messages, 'doneReport', '«idAndDoneResult»',
+        row.identifier + ' ' + row.what
+      );
+    }
+  }
+  // Otherwise, i.e. if no task was removed:
+  else {
+    // Report this.
+    handleMessage(messages, 'doneMissingFail');
+  }
+};
+
+/// Define a handler for the result of the list command.
+const listHandler = (messages, result) => {
+  // Create a table with column headers for list items.
+  const table = new Table({
+    head: [messages.listCol0Head, messages.listCol1Head]
+  });
+  // Identify an array of the row objects in the result, in ID order.
+  const rows = result.rows;
+  // If any tasks exist:
+  if (rows.length) {
+    // For each of them:
+    for (const row of rows) {
+      // Add it to the table.
+      table.push([row.identifier, row.what]);
+    }
+  }
+  // Identify a footer template for the table.
+  const footer =
+    rows.length === 1 ? messages.listSumReport1 : messages.listSumReport2;
+  /*
+    Identify a custom messages object for this command’s report.
+  */
+  const listMessages = {'table': [table.toString(), footer].join('\n')};
+  // Handle the table and its footer template as a message.
+  handleMessage(listMessages, 'table', '«rows.length»', rows.length);
+};
+
+/// Define a handler for the result of the reset command.
+const resetHandler = (messages) => {
+  handleMessage(messages, 'resetReport');
+};
+
+const {helpHandler, callFn, handleMessage, mkFnCall, formulateMessage}
+  = module.exports;
---
+++ src/util.js
@@ -0,0 +1,126 @@
+/**
+  Management utilities for a list stored in a PostgreSQL database. Performs
+  additions to it, deletions from it, and disclosures and resets of it.
+*/
+
+/// ///// IMPORTS ///// ///
+
+const {Client} = require('pg');
+
+const Table = require('cli-table2');
+
+/// ///// GENERAL FUNCTIONS ///// ///
+
+/**
+  Define a function that returns a database function invocation.
+  Preconditions:
+    0. args has length 1 or more.
+    1. Each element of args is defined.
+    2. args[0] is a valid PostgreSQL identifier.
+*/
+const mkFnCall = args => {
+  const fnArgs = args.slice(1).map(arg => "'" + arg + "'").join(', ');
+  return 'select * from ' + args[0] + '(' + fnArgs + ')';
+};
+
+/**
+  Define a function that formulates a message.
+  Preconditions:
+    0. messages is an object.
+    1. messageKey is one of the properties of messages.
+    2. symbol is a nonblank string without any RegExp metacharacters.
+    3. replacement is a string.
+*/
+const formulateMessage = (messages, messageKey, symbol, replacement) => {
+  let message = messages[messageKey];
+  if (symbol) {
+    message = message.replace(RegExp(symbol, 'g'), replacement);
+  }
+  return message;
+};
+
+/// Define a function that acts on a message.
+const handleMessage = (messages, messageKey, symbol, replacement) => {
+  console.log(formulateMessage(messages, messageKey, symbol, replacement));
+};
+
+/**
+  Define a function that connects to the tasks database, executes a function
+  in it, handles its result, and disconnects from the database.
+*/
+const callFn = (messages, handler, fnName, ...fnArgs) => {
+  const client = new Client({
+    user: 'taskmaster',
+    database: 'tasks'
+  });
+  client.connect().then(
+    () => {
+      return client.query(mkFnCall([fnName, ...fnArgs]));
+    }
+  ).then(
+    result => {
+      handler(messages, result);
+      return client.end();
+    }
+  ).catch(
+    error => {
+      console.log('Error (' + fnName + '): ' + error.message);
+      client.end();
+    }
+  );
+};
+
+/// ///// COMMAND-SPECIFIC HANDLERS ///// ///
+
+/// Define a handler for the result of the help command.
+const helpHandler = messages => {
+  handleMessage(messages, 'helpTip');
+};
+
+/// Define a handler for the result of the add command.
+const addHandler = (messages, result) => {
+  handleMessage(
+    messages, 'addReport', '«addResult»', result.rows[0].identifier
+  );
+};
+
+/// Define a handler for the result of the done command.
+const doneHandler = (messages, result) => {
+  const rows = result.rows;
+  if (rows.length) {
+    for (const row of rows) {
+      handleMessage(
+        messages, 'doneReport', '«idAndDoneResult»',
+        row.identifier + ' ' + row.what
+      );
+    }
+  }
+  else {
+    handleMessage(messages, 'doneMissingFail');
+  }
+};
+
+/// Define a handler for the result of the list command.
+const listHandler = (messages, result) => {
+  const table = new Table({
+    head: [messages.listCol0Head, messages.listCol1Head]
+  });
+  const rows = result.rows;
+  if (rows.length) {
+    for (const row of rows) {
+      table.push([row.identifier, row.what]);
+    }
+  }
+  const footer =
+    rows.length === 1 ? messages.listSumReport1 : messages.listSumReport2;
+  const listMessages = {'table': [table.toString(), footer].join('\n')};
+  handleMessage(listMessages, 'table', '«rows.length»', rows.length);
+};
+
+/// Define a handler for the result of the reset command.
+const resetHandler = (messages) => {
+  handleMessage(messages, 'resetReport');
+};
+
+const {helpHandler, callFn, handleMessage, mkFnCall, formulateMessage}
+  = module.exports;
